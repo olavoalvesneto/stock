8c502eedb7662087f8f6939ea63c6ff5bf2487a2
Olavo Santos <olavo.santos@vistajet.com>
2020-10-12 20:42:09 -0300

    adicionei atualizado logica do http

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -2,10 +2,7 @@ import axios from 'axios'
 import { store } from '../redux'
 
 const http = axios.create({
-   baseURL: 'http://127.0.0.1:3024',
-   headers: {
-    authorization: 'Bearer v3ryt0ps3cr3tt0k3n#432'
-   }
+   baseURL: 'http://localhost:3024',
 })
 
 http.interceptors.request.use(config => {
